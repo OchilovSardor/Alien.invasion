5f40bbb09b8eb9c8c48805961a8d0c908be11acd
Ochilov Sardor <ocilovs425@gmail.com>
2025-12-20 22:32:30 +0500

    Add color to background

--- sentral.py
+++ sentral.py
@@ -11,8 +11,11 @@ class AlienInvasion:
         """Initialise the game and create resourses."""
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((1000, 700))
         pygame.display.set_caption("Alien Invasion")
+
+        #Set the background color
+        self.bg_color = (10, 10, 40)
     
     def run_game(self):
         """Start the main loop for the game."""
@@ -21,6 +24,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            #Redraw the screen during the each pass through the loop
+            self.screen.fill(self.bg_color)
             
             #Make the most recently drawn screen visible
             pygame.display.flip()
